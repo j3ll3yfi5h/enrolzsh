440c0e50efb156970dca8a3b5978011fc607515e
Andrew Jelley <j3ll3yfi5h@users.noreply.github.com>
2017-04-21 22:33:03 +0900

    changed commands so they can be used on WSL

--- vagrant.zsh
+++ vagrant.zsh
@@ -1,8 +1,8 @@
 # Vagrant Command
 #
 
-enrol.vu() { eval cd $ER_VM_DIR && vagrant up }
-enrol.vh() { eval cd $ER_VM_DIR && vagrant halt }
-enrol.vr() { eval cd $ER_VM_DIR && vagrant reload }
-enrol.vs() { eval cd $ER_VM_DIR && vagrant ssh }
-enrol.vb() { eval cd $ER_VM_DIR && vagrant box update }
+enrol.vu() { eval cd $ER_VM_DIR && cmd.exe /C "vagrant up" }
+enrol.vh() { eval cd $ER_VM_DIR && cmd.exe /C "vagrant halt" }
+enrol.vr() { eval cd $ER_VM_DIR && cmd.exe /C "vagrant reload" }
+enrol.vs() { eval cd $ER_VM_DIR && cmd.exe /C "vagrant ssh" }
+enrol.vb() { eval cd $ER_VM_DIR && cmd.exe /C "vagrant box update" }
